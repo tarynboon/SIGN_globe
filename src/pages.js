9138b5e5874a846774b17a92ad38bf241b47a91e
Taryn Boonpongmanee <tarynboon4@gmail.com>
2025-12-22 19:00:00 -0800

    Add Pages entry point

---
+++ src/pages.js
@@ -0,0 +1,7 @@
+import { mountSignGlobe } from "./mount.js"; 
+// 👆 change this filename ONLY if your function lives elsewhere
+
+mountSignGlobe({
+  containerId: "sign-globe",
+  height: "100vh"
+});
